097a72093923b78fb604fafb89f3f9986abc3a8f
Johnny Pesola <johnny@pesola.eu>
2015-10-29 21:28:36 +0100

    There is a wizard hiding in your wardrobe

--- BlackJack/view/SwedishView.cs
+++ BlackJack/view/SwedishView.cs
@@ -11,14 +11,18 @@ namespace BlackJack.view
         List<model.IBlackJackObserver> m_observers;
         public void AddSubscribers(model.IBlackJackObserver a_sub)
         {
+            if (m_observers == null)
+            {
+                m_observers = new List<model.IBlackJackObserver>();
+            }
             m_observers.Add(a_sub);
         }
+
         public void DisplayWelcomeMessage()
         {
             System.Console.Clear();
             System.Console.WriteLine("Hej Black Jack Världen");
-            System.Console.WriteLine("----------------------");
-            System.Console.WriteLine("Skriv 'p' för att Spela, 'h' för nytt kort, 's' för att stanna 'q' för att avsluta\n");
+            System.Console.WriteLine("Skriv 'p' för att Spela, 'h' för nytt kort, 's' för att stanna 'q' för avsluta\n");
         }
 
         public void DisplayHands()
@@ -49,51 +53,45 @@ namespace BlackJack.view
                     return model.Game.Status.Undefined;
             }
         }
+
         public void DisplayCard(model.Card a_card)
         {
-            if (a_card.GetColor() == model.Card.Color.Hidden)
-            {
-                System.Console.WriteLine("Dolt Kort");
-            }
-            else
-            {
-                String[] colors = new String[(int)model.Card.Color.Count]
-                    { "Hjärter", "Spader", "Ruter", "Klöver" };
-                String[] values = new String[(int)model.Card.Value.Count] 
-                    { "två", "tre", "fyra", "fem", "sex", "sju", "åtta", "nio", "tio", "knekt", "dam", "kung", "ess" };
-                System.Console.WriteLine("{0} {1}", colors[(int)a_card.GetColor()], values[(int)a_card.GetValue()]);
-            }
+            System.Console.WriteLine("{0} of {1}", a_card.GetValue(), a_card.GetColor());
         }
+
         public void DisplayPlayerHand(IEnumerable<model.Card> a_hand, int a_score)
         {
             DisplayHand("Spelare", a_hand, a_score);
         }
+
         public void DisplayDealerHand(IEnumerable<model.Card> a_hand, int a_score)
         {
-            DisplayHand("Croupier", a_hand, a_score);
-        }
-        public void DisplayGameOver(bool a_dealerIsWinner)
-        {
-            System.Console.Write("Slut: ");
-            if (a_dealerIsWinner)
-            {
-                System.Console.WriteLine("Croupiern Vann!");
-            }
-            else
-            {
-                System.Console.WriteLine("Du vann!");
-            }
+            DisplayHand("Dealern", a_hand, a_score);
         }
 
         private void DisplayHand(String a_name, IEnumerable<model.Card> a_hand, int a_score)
         {
-            System.Console.WriteLine("{0} Har: ", a_name);
+            System.Console.WriteLine("{0} har: ", a_name);
             foreach (model.Card c in a_hand)
             {
                 DisplayCard(c);
             }
             System.Console.WriteLine("Poäng: {0}", a_score);
             System.Console.WriteLine("");
+        }
+
+        public void DisplayGameOver(bool a_dealerIsWinner)
+        {
+            System.Console.Write("GameOver: ");
+            if (a_dealerIsWinner)
+            {
+                System.Console.WriteLine("Dealern vann!");
+            }
+            else
+            {
+                System.Console.WriteLine("Du vann!");
+            }
+
         }
     }
 }
